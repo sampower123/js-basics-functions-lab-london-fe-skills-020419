057f968eed8672fdc00a3575e0a184304b4e5add
Sam Power <sam.g.power@gmail.com>
2019-03-06 20:03:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const hq = 42
+
+function distanceFromHqInBlocks(pickup) {
+  return Math.abs(hq - pickup); 
+}
+
+function distanceFromHqInFeet(feet) {
+  return distanceFromHqInBlocks(feet)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs((start - destination)*264);
+}
+
+function calculatesFarePrice(fare) {
+  if (distanceTravelledInFeet(fare) > 400 && < 2000 {
+   return (fare-400)*2;
+  }
+  else if () {
+  if (distanceTravelledInFeet(fare) > 2000 && < 2500 {
+  return "$25";
+    }
+  }
+  else {
+    return "";
+  }
+}
