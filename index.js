416dd9823fec4a027198e4568f16db591d76ec9d
Sam Power <sam.g.power@gmail.com>
2019-03-06 20:36:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,4 +21,7 @@ function calculatesFarePrice(start, destination) {
   else if (distanceTravelledInFeet(start, destination) > 400) {
     return (distanceTravelledInFeet(start, destination)-400)*.02;
   }
+  else if (distanceTravelledInFeet(start, destination) > 2000) {
+    return (distanceTravelledInFeet(start, destination)-400)*.02;
+  }
 }
