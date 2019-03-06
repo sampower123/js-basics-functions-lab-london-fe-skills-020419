23336da519aac930f5803f043252658e428c1e25
Sam Power <sam.g.power@gmail.com>
2019-03-06 20:03:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,16 +14,3 @@ function distanceTravelledInFeet(start, destination) {
   return Math.abs((start - destination)*264);
 }
 
-function calculatesFarePrice(fare) {
-  if (distanceTravelledInFeet(fare) > 400 && < 2000 {
-   return (fare-400)*2;
-  }
-  else if () {
-  if (distanceTravelledInFeet(fare) > 2000 && < 2500 {
-  return "$25";
-    }
-  }
-  else {
-    return "cannot travel that far";
-  }
-}
